0b4e43bc4e82c2eeaee8c6d7f77eedca38f93749
kiriti chunduri <kc@kiritis-MacBook-Pro.local>
2024-03-04 20:59:04 +0530

    Adding new subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,13 +11,23 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
   tags = {
-    Name = "PublicSubnet"
+    Name = "PublicSubnet1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "PublicSubnet2"
   }
 }
 
@@ -85,7 +95,7 @@ resource "aws_lb" "microservice_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.microservice.id]
-  subnets            = [aws_subnet.public.id]
+  subnets            = [aws_subnet_1.public.id, aws_subnet.public_2.id]
 
   enable_deletion_protection = false
 
