96adac49e98f69b36bcffb4e9ab8d68e989a2d45
kiriti chunduri <kc@kiritis-MacBook-Pro.local>
2024-03-04 20:52:51 +0530

    Adding Cloudwatch and load balancers

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,3 +79,55 @@ resource "aws_instance" "microservice_instance" {
     Name = "MicroserviceInstance"
   }
 }
+
+resource "aws_lb" "microservice_alb" {
+  name               = "microservice-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.microservice.id]
+  subnets            = [aws_subnet.public.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "MicroserviceALB"
+  }
+}
+
+resource "aws_lb_target_group" "microservice_tg" {
+  name     = "microservice-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled = true
+    path    = "/"
+    port    = "traffic-port"
+    protocol= "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "microservice_listener" {
+  load_balancer_arn = aws_lb.microservice_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.microservice_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "microservice_tg_attachment" {
+  target_group_arn = aws_lb_target_group.microservice_tg.arn
+  target_id        = aws_instance.microservice_instance.id
+  port             = 80
+}
+
+resource "aws_cloudwatch_log_group" "microservice_log_group" {
+  name = "/aws/microservice/logs"
+
+  retention_in_days = 14
+}
+
