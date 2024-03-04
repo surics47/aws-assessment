bbba78981a3e12c1d2b068df6b0ee7791e26b8f8
kiriti chunduri <kc@kiritis-MacBook-Pro.local>
2024-03-04 20:11:01 +0530

    Adding terraform code

---
+++ infrastructure/main.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MicroservicesVPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_security_group" "microservice" {
+  name        = "microservice_sg"
+  description = "Allow microservice traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "microservice_role" {
+  name = "microservice_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_route53_zone" "microservice_dns" {
+  name = "microservice.local"
+}
+
+resource "aws_ec2_instance" "microservice_instance" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.microservice.name]
+
+  iam_instance_profile {
+    name = aws_iam_role.microservice_role.name
+  }
+
+  tags = {
+    Name = "MicroserviceInstance"
+  }
+}
+
